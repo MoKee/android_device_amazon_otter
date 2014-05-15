654685d48f16d6163abfdb20f9618916b177481f
martincz <martincz@mokeedev.com>
2014-05-15 12:59:55 +0800

    mokee bringup
    Change-Id: I8b3275482314a8e9132af68049d1b62756c95647

--- mokee.mk
+++ mokee.mk
@@ -1,19 +1,21 @@
-#
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2013 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-add_lunch_combo full_otter-userdebug
-add_lunch_combo cm_otter-userdebug
+# Inherit device configuration for Kindle Fire
+$(call inherit-product, device/amazon/otter-common/mokee.mk)
+$(call inherit-product, device/amazon/otter/full_otter.mk)
+
+PRODUCT_NAME := mk_otter
+PRODUCT_RELEASE_NAME := KFire
 
